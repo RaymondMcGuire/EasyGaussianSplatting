ae16c9abb6155a9e6d4af198191b190630858c62
raymondmg <wang.xu@ist.hokudai.ac.jp>
2026-02-02 11:17:31 +0900

    update

--- scripts/viewer-3dgs.bat
+++ scripts/viewer-3dgs.bat
@@ -2,8 +2,8 @@ cd ..
 call conda activate easygsplat
 set KMP_DUPLICATE_LIB_OK=TRUE
 
-@REM python gaussian_viewer.py --gs="D:/GS-project/GSModels/bicycle/point_cloud/iteration_30000/point_cloud.ply"
+python gaussian_viewer.py --gs="D:/unity-project/library/VR-GS/VR-GS/Data/dance_siyu/_0_point_cloud.ply"
 
-python gaussian_viewer.py --gs="D:/GS-project/MultiLayer-3DGS/Multi-Layer-Anatomy-GS-Training/eval/zanatomy-muscular/point_cloud/iteration_30000/point_cloud.ply"
+REM python gaussian_viewer.py --gs="D:/GS-project/MultiLayer-3DGS/Multi-Layer-Anatomy-GS-Training/eval/zanatomy-muscular/point_cloud/iteration_30000/point_cloud.ply"
 
 pause
